7f06a6972ee41f0c6f1c0e460968cd06ed0adcfb
Audra Tallman <audra.tallman@hotmail.com>
2019-02-25 20:28:42 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,11 +4,3 @@ def square_array(array)
   array.each { |element| squared << element **2}
   squared
 end
-
-new_numbers = [9,10,16,25]
-
-def square_array(new_numbers)
-  squared = []
-  array.each { |element| squared << element **2}
-  squared
-end
